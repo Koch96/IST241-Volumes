ddbb3f54a5cc590f2c8b07bda265be166e68afc9
Stephen <stephen.koch96@gmail.com>
2021-10-19 14:29:30 -0400

    finished volumes + cones and Shape classes

--- src/volumes/Volumes.java
+++ src/volumes/Volumes.java
@@ -5,11 +5,15 @@ package volumes;
 public class Volumes {
 
     public static void main(String[] args) {
+        double radius = 0;
+        double height = 0;
         Cube myCube = new Cube();
+        Cone myCone = new Cone(radius, height);
         
-        myCube.setSide(5);
+//        myCube.setSide(5);
+//        System.out.println("The volume of the cube is: " + myCube.getVolume());
         
-        System.out.println("The volume of the cube is: " + myCube.getVolume());
+        System.out.println(myCone.calcVolume(15, 20));
     }
 
 }
